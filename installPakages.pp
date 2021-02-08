0891412c1781278cc96afed01b623879f9078861
Ashraf <test@test.com>
2021-02-09 01:37:07 +0200

    Create installPakages.pp

---
+++ installPakages.pp
@@ -0,0 +1,17 @@
+# install pakages based on the OS family
+
+class packages {
+   package { 'python-requests':
+       ensure => installed,
+   }
+   if $facts[os][family] == "Debian" {
+     package { 'golang':
+       ensure => installed,
+     }
+  }
+   if $facts[os][family] == "RedHat" {
+     package { 'nodejs':
+       ensure => installed,
+     }
+  }
+}
